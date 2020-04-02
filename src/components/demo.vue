af7ed4320e8b5fec5b47ec3ed8bc4262bed3175b
xufuxiang <1936450992@qq.com>
2020-04-02 13:16:43 +0800

    测试

---
+++ src/components/demo.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="">
+  这是一个测试文件
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
